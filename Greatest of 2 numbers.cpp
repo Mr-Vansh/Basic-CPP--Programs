57d346d152caea4a8c92d648b1a75f436020db82
Vansh Chitlangia <85049151+Mr-Vansh@users.noreply.github.com>
2021-10-09 17:24:48 +0530

    Create Greatest of 2 numbers.cpp

---
+++ Greatest of 2 numbers.cpp
@@ -0,0 +1,23 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+    
+    int num1;
+    cout<<"Enter first number : ";
+    cin>>num1;
+    
+    int num2;
+    cout<<"Enter second number : ";
+    cin>>num2;
+    
+    if(num1>num2)
+    {
+        cout<<"FIRST Number is greater.";
+    }
+    else
+    {
+        cout<<"SECOND number is greater.";
+    }
+    return 0;
+}
