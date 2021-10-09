13f2a3242e39098deb8df0748ca0273be83163ed
Vansh Chitlangia <85049151+Mr-Vansh@users.noreply.github.com>
2021-10-09 17:14:48 +0530

    Create Even or Odd.cpp

---
+++ Even or Odd.cpp
@@ -0,0 +1,20 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+    
+    int number;
+    cout<<"Enter any number : ";
+    cin>>number;
+    
+    if(number%2==0)
+    {
+        cout<<"It's an EVEN number.";
+    }
+    else
+    {
+        cout<<"It's an ODD number.";
+    }
+    
+    return 0;
+}
